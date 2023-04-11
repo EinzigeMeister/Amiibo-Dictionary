b86b19c7543747123b94c6f116b091d05cae029d
Eric Krukowski <erickruk@amazon.com>
2023-04-11 03:51:08 -0400

    Add goals as comments

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ while(gameFilter.length>0){
     gameFilter.removeChild(gameFilter.firstElementChild)
 }
 */
+//1. generate amiibo object list
+//2. generate filter options
+//3. set first filter option to selected
+//4. display first filter options characters
+//5. show the first character's amiibo on the amiibo container
+//5.a. update amiibo name and image
+//5.b. update amiibo game list (left & right)
+//5.c. select first game's usage and display under image
+//6. add filter 'change' action event to update characters based on selected game
+//7. add game 'click' action event to update amiibo based on selected game
+//8. stretch: add 'hover' action to rotate/wiggle amiibo when hovered
 fetch(fetchGameSeriesURL).then(resp=>resp.json()).then(seriesList=>{
     //generate filter options
     const series = []
     for (item of seriesList.amiibo){
         //only adds new names
-        if (series.indexOf(item.name)==-1){
+        if (series.indexOf(item.name)===-1){
             series.push(item.name)
             const optionElement = document.createElement('option')
+            if(series.length===1) optionElement.setAttribute('selected','true')
             optionElement.innerHTML=item.name
             optionElement.value=item.name
             gameFilter.append(optionElement)
         }
     }
+    
 })
