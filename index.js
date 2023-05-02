f5def89c0aca5db0c2e0511d6e2f9af93b256ecf
Eric Krukowski <erickruk@amazon.com>
2023-05-01 22:25:33 -0400

    format code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log(`For more information about data drawn from this page, see https://www.amiiboapi.com/docs/`)
 const gameFilter = document.getElementById('sidebar').querySelector('select');
 //Use filter to select game series and populate characters
-gameFilter.addEventListener('change', e=>{
+gameFilter.addEventListener('change', e => {
     clearCharacterList()
     refreshCharacterList(e.target.value)
 })
@@ -12,27 +12,27 @@ let gameList = []
 
 //Generate list of game series' to update filter 
 const gameOptionNodes = document.querySelectorAll('option')
-const gameOptions =[]
-gameOptionNodes.forEach(option=>gameOptions.push(option.value))
- gameOptions.sort();
- clearGameFilter();
- refreshGameFilter();
+const gameOptions = []
+gameOptionNodes.forEach(option => gameOptions.push(option.value))
+gameOptions.sort();
+clearGameFilter();
+refreshGameFilter();
 
 //obtain amiibos. The fetch from https://www.amiiboapi.com/api/amiibo/?showusage was used to generate local db
-fetch("http://localhost:3000/amiibo").then(resp=>resp.json()).then(amiiboObjs=>{
-    amiiboLib=amiiboObjs
+fetch("http://localhost:3000/amiibo").then(resp => resp.json()).then(amiiboObjs => {
+    amiiboLib = amiiboObjs
     refreshCharacterList(gameFilter.value)
 })
 
 //Helper functions
-function clearGameFilter(){
-    while(gameFilter.length>0){
+function clearGameFilter() {
+    while (gameFilter.length > 0) {
         gameFilter.removeChild(gameFilter.firstElementChild)
     }
 }
 //update the filter if any new game series are added
-function refreshGameFilter(){
-    gameOptions.forEach(option=>{
+function refreshGameFilter() {
+    gameOptions.forEach(option => {
         const newOption = document.createElement('option')
         newOption.textContent = option
         newOption.value = option
@@ -43,20 +43,20 @@ function refreshGameFilter(){
 }
 
 //empties current character list
-function clearCharacterList(){
+function clearCharacterList() {
     characterList.replaceChildren()
 }
 
-function refreshCharacterList(seriesName){
+function refreshCharacterList(seriesName) {
     //creates a modified array of selected filter
-    filteredCharacters=[]
-    filteredCharacters = amiiboLib.filter(amiibo=>{
-        if(amiibo.gameSeries.localeCompare(seriesName)==0)  return true
+    filteredCharacters = []
+    filteredCharacters = amiiboLib.filter(amiibo => {
+        if (amiibo.gameSeries.localeCompare(seriesName) == 0) return true
         else return false
     })
     filteredCharacters.sort((a, b) => a.name.localeCompare(b.name))
     //displays each character from the array
-    filteredCharacters.forEach(character=>{
+    filteredCharacters.forEach(character => {
         const characterLi = document.createElement('li')
         const characterButton = document.createElement('button')
         characterButton.textContent = character.name
@@ -67,8 +67,8 @@ function refreshCharacterList(seriesName){
     //Inializes the first character as the displayed Amiibo
     resetSelectedAmiibo(filteredCharacters[0])
 }
-function resetSelectedAmiibo(character){
-    if(character==undefined)return
+function resetSelectedAmiibo(character) {
+    if (character == undefined) return
     clearGameList('3ds')
     clearGameList('switch')
     clearGameList('wii-u')
@@ -76,55 +76,55 @@ function resetSelectedAmiibo(character){
     nullGame = {
         gameName: 'None',
         amiiboUsage: {
-            usage:'No game selected. Select a game to show usage'
+            usage: 'No game selected. Select a game to show usage'
         }
     }
-    document.getElementById('amiibo-image').src=character.image
-    document.getElementById('amiibo-name').textContent=character.name
+    document.getElementById('amiibo-image').src = character.image
+    document.getElementById('amiibo-name').textContent = character.name
     firstDisplayed = false
     //check each console games from object and add them to the list, if none are found, add a default "none" game. Display the first game's usage
-    if(character.gamesSwitch.length>0){
-        character.gamesSwitch.forEach(game=>addGame(game, 'switch'))
+    if (character.gamesSwitch.length > 0) {
+        character.gamesSwitch.forEach(game => addGame(game, 'switch'))
         updateUsage(character.gamesSwitch[0])
         firstDisplayed = true
     }
     else addGame(nullGame, 'switch')
 
-    if(character.games3DS.length>0){
-        character.games3DS.forEach(game=>addGame(game, '3ds'))
-        if(!firstDisplayed){
-           updateUsage(character.games3DS[0])
-           firstDisplayed = true
+    if (character.games3DS.length > 0) {
+        character.games3DS.forEach(game => addGame(game, '3ds'))
+        if (!firstDisplayed) {
+            updateUsage(character.games3DS[0])
+            firstDisplayed = true
         }
     }
     else addGame(nullGame, '3ds')
 
-    if(character.gamesWiiU.length>0){
-        character.gamesWiiU.forEach(game=>addGame(game, 'wii-u'))
-        if(!firstDisplayed){
+    if (character.gamesWiiU.length > 0) {
+        character.gamesWiiU.forEach(game => addGame(game, 'wii-u'))
+        if (!firstDisplayed) {
             updateUsage(character.games3DS[0])
             firstDisplayed = true
-         }
+        }
     }
     else addGame(nullGame, 'wii-u')
-    
+
 }
-function clearGameList(system){
+function clearGameList(system) {
     const games = document.getElementById(`game-list-${system}`)
-    games.replaceChildren(`${system}`.toUpperCase())    
+    games.replaceChildren(`${system}`.toUpperCase())
 }
-function addGame(game, system){
+function addGame(game, system) {
     const newGameObj = document.createElement('p')
     newGameObj.textContent = game.gameName
     //only add event listeners for valid games
-    if(game.gameName.localeCompare('None')!=0){
+    if (game.gameName.localeCompare('None') != 0) {
         newGameObj.addEventListener('click', updateUsage.bind(null, game))
     }
     document.getElementById(`game-list-${system}`).append(newGameObj)
 }
 //updates the usage text for the selected game
-function updateUsage(game){
+function updateUsage(game) {
     let usageText = game.amiiboUsage[0].Usage
-    usageText = usageText.slice(0,1).toLowerCase() + usageText.slice(1)
-    document.getElementById('amiibo-usage').textContent= `In ${game.gameName}, you can ${usageText}`
+    usageText = usageText.slice(0, 1).toLowerCase() + usageText.slice(1)
+    document.getElementById('amiibo-usage').textContent = `In ${game.gameName}, you can ${usageText}`
 }
